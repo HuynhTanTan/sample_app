3296de87c83d772bb09afcac6fdba37650ede9bf
HuynhTanTan <huynhtantan451996@gmail.com>
2018-06-06 12:35:34 +0700

    fix chapter 3 lan 2

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,8 @@
-require 'test_helper'
+require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
-  end
-
-  test "should get root" do
-    get root_url
-    assert_response :success
   end
 
   test "should get home" do
@@ -32,6 +26,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
-
 end
